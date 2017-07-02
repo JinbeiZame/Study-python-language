e18062b0464dac43a21c820dc31f85195c8025c9
jinbeizame <sirasit.khongsangdao@gmail.com>
2017-07-03 00:55:24 +0700

    only if,elif

---
+++ cut_word.py
@@ -0,0 +1,7 @@
+import string
+
+sentence = "  sam is running into the room  "
+
+print(str(len(sentence)))
+print(str(sentence.strip(" ")))
+print(str(len(sentence.strip(" "))))
---
+++ if_else.py
@@ -0,0 +1,17 @@
+#ถ้าผลลัพธฺ์ เป็นจริงจะสิ้นสุด
+month1 = 2
+month2 = 10
+month3 = 15
+month4 = 20
+month5 = 25
+if  month1 > month2 :
+        print(month1)
+elif month1 < month3 :
+        print("month 1 less than month3")
+elif month3 > month4 :
+        print("month3 less than month4")
+elif month5 > month4 :
+        print("month5 more than month4 ")
+else:
+    print(month1)
+    #print("month2" + "is " + month2 +"less than " + month1)
